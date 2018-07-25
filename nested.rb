7f3b36ed9ecb7841616b13c6645a3f44a50434ab
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-25 19:48:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -18,6 +18,8 @@ def hopper
   programmer_hash[:grace_hopper]
 end
 
+
+
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
@@ -39,6 +41,8 @@ def alan_kay_is_known_for
   programmer_hash[:alan_kay][:known_for]
 end
 
+
+
 def dennis_ritchies_language
 	programmer_hash = 
  		{
@@ -57,6 +61,8 @@ def dennis_ritchies_language
      }
   programmer_hash[:dennis_ritchie][:languages].first
 end
+
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -89,6 +95,8 @@ def adding_matz
     programmer_hash
 end
 
+
+
 def changing_alan
 	programmer_hash = 
  		{
@@ -108,8 +116,11 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+      programmer_hash[:alan_kay][:known_for] = "GUI"
+      programmer_hash
 end
+
+
 
 def adding_to_dennis
 	programmer_hash = 
